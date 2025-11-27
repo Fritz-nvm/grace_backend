8c165e2e1c5e5734500c004aa841cbcdeee4ef15
Fritz-nvm <fritzimpah@gmail.com>
2025-11-27 06:59:36 +0100

    fix config

--- app/config.py
+++ app/config.py
@@ -3,11 +3,11 @@ from functools import lru_cache
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:superuser@localhost:5433/grace"
-    SECRET_KEY: str = "ghggy88uy6r5efo[-90836234excu7u900jpj0908i989h]"
-    ENVIRONMENT: str = "development"
-    PROJECT_NAME: str = "Clothing Brand API"
-    API_V1_STR: str = "/api/v1"
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ENVIRONMENT: str
+    PROJECT_NAME: str
+    API_V1_STR: str
 
     class Config:
         env_file = ".env"
